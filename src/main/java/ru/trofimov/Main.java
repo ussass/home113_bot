0238c8cc5442b2665a6e36de4c4d2e9a2f341910
Mikle <ussass113@gmail.com>
2020-09-07 11:23:07 +0300

    Let's start

--- src/main/java/ru/trofimov/Main.java
+++ src/main/java/ru/trofimov/Main.java
@@ -10,38 +10,30 @@ import java.awt.event.ActionListener;
 public class Main {
     public static void run()
     {
-        //Создадим окно и установим заголовок
         final JFrame window = new JFrame("Caption");
 
-        //Подключаем иконку из корня папки проекта
         ImageIcon img = new ImageIcon("java.png");
         window.setIconImage(img.getImage());
 
-        //Событие "закрыть" при нажатии по крестику окна
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Текстовое поле
         JTextField textField = new JTextField();
         textField.setBackground(Color.WHITE);
         textField.setColumns(14);
 
 
-        //Создадим панель
         JPanel panel = new JPanel();
 
-        //Создадим кнопки
         JButton StartButton = new JButton("Start");
         JButton exitButton = new JButton("Exit");
 
         final JLabel label = new JLabel("Bot is not running         ");
 
 
-        //Событие для кнопки "Свернуть"
         StartButton.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
             {
-                //Действие
                 if (Bot.startBot()) {
                     label.setText("Bot is working               ");
                 } else {
@@ -50,12 +42,10 @@ public class Main {
             }
         });
 
-        //Событие для кнопки "Выход"
         exitButton.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
             {
-                //Действие
                 window.setVisible(false);
                 System.exit(0);
             }
@@ -63,23 +53,19 @@ public class Main {
 
 
 
-        //Добавим кнопки и поля на панель
         panel.add(StartButton);
         panel.add(label);
         panel.add(exitButton);
 
 
-        //Добавим панель в окно
         window.getContentPane().add(panel);
 
         window.pack();
 
-        //Разместим программу по центру
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
 
-    //Запускаем
     public static void main(String[] args)
     {
         run();
